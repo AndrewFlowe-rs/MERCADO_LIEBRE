9a1261f6688d320c8eece7e93909f9e6c77fd35f
andrewflower-rs <arnaldomahone@hotmail.com>
2024-02-15 02:18:38 -0300

    agrego los respectivos formularios

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ app.use(express.static('public'))
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+app.get('/ingresar', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
 
 app.listen(port,() =>console.log('http://localhost:3030' )) 
